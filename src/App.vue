952ac59da43dfe39340825f64ab1b3d51387fae8
Kai-suzuki1 <kaisuzuki1995@gmail.com>
2024-06-03 19:40:02 +0900

    Refactored to comment out unnecessary fall-out pages

--- src/App.vue
+++ src/App.vue
@@ -1,62 +1,56 @@
 <template>
   <div class="h-screen">
-    <template v-if="isNotFound">
+    <!-- <template v-if="isNotFound">
       <NotFound />
     </template>
     <template v-else-if="isSystemError">
       <InternalServerError />
-    </template>
-    <template v-else>
-      <RouterView
-        name="Header"
-        v-slot="{ Component, route }"
-      >
-        <template v-if="Component">
-          <Transition
-            mode="out-in"
-            enter-active-class="transition-opacity"
-            leave-active-class="transition-opacity"
-            enter-from-class="opacity-0"
-            leave-to-class="opacity-0"
-          >
-            <Suspense timeout="0">
-              <div :key="route.fullPath">
-                <component :is="Component" />
-              </div>
-              <template #fallback>Loading...</template>
-            </Suspense>
-          </Transition>
-        </template>
-      </RouterView>
-      <RouterView v-slot="{ Component, route }">
-        <template v-if="Component">
-          <Transition
-            mode="out-in"
-            enter-active-class="transition-opacity"
-            leave-active-class="transition-opacity"
-            enter-from-class="opacity-0"
-            leave-to-class="opacity-0"
-          >
-            <Suspense timeout="0">
-              <div :key="route.fullPath">
-                <component :is="Component" />
-              </div>
-              <template #fallback>Loading...</template>
-            </Suspense>
-          </Transition>
-        </template>
-      </RouterView>
-    </template>
+    </template> -->
+    <RouterView
+      name="Header"
+      v-slot="{ Component, route }"
+    >
+      <template v-if="Component">
+        <Transition
+          mode="out-in"
+          enter-active-class="transition-opacity"
+          leave-active-class="transition-opacity"
+          enter-from-class="opacity-0"
+          leave-to-class="opacity-0"
+        >
+          <!-- <Suspense timeout="0"> -->
+          <div :key="route.fullPath">
+            <component :is="Component" />
+          </div>
+          <!-- <template #fallback>Loading...</template> -->
+          <!-- </Suspense> -->
+        </Transition>
+      </template>
+    </RouterView>
+    <RouterView v-slot="{ Component, route }">
+      <template v-if="Component">
+        <Transition
+          mode="out-in"
+          enter-active-class="transition-opacity"
+          leave-active-class="transition-opacity"
+          enter-from-class="opacity-0"
+          leave-to-class="opacity-0"
+        >
+          <Suspense timeout="0">
+            <div :key="route.fullPath">
+              <component :is="Component" />
+            </div>
+            <template #fallback>Loading...</template>
+          </Suspense>
+        </Transition>
+      </template>
+    </RouterView>
   </div>
 </template>
 
 <script setup lang="ts">
-  import { storeToRefs } from 'pinia'
   import { RouterView } from 'vue-router'
-  import { useErrorFlagStore } from './stores/useErrorFlagStore'
-  import InternalServerError from './views/InternalServerError.vue'
-  import NotFound from './views/NotFound.vue'
 
-  const store = useErrorFlagStore()
-  const { isNotFound, isSystemError } = storeToRefs(store)
+  // const store = useErrorFlagStore()
+  // const { isNotFound, isSystemError } = storeToRefs(store)
 </script>
